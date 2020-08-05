ed7209392de88e4e8a197a034e9402d96545120a
ZerichaHidara <fendyakakom@gmail.com>
2020-08-06 02:31:01 +0700

    First init

--- app/src/main/java/com/e/rabela/MainActivity.kt
+++ app/src/main/java/com/e/rabela/MainActivity.kt
@@ -2,11 +2,26 @@ package com.e.rabela
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.e.rabela.fragment.AkunFragment
+import com.e.rabela.fragment.HomeFragment
+import com.e.rabela.fragment.KeranjangFragment
 
 class MainActivity : AppCompatActivity() {
+
+    val  fragmentHome: Fragment = HomeFragment()
+    val  fragmentAkun: Fragment = AkunFragment()
+    val  fragmentKerangjang: Fragment = KeranjangFragment()
+    val  fm: FragmentManager = supportFragmentManager
+    var  active: Fragment = fragmentHome
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fm.beginTransaction().add(R.id.nav_host_fragment, fragmentHome).show(fragmentHome).commit()
+        fm.beginTransaction().add(R.id.nav_host_fragment, fragmentAkun).hide(fragmentAkun).commit()
+        fm.beginTransaction().add(R.id.nav_host_fragment, fragmentKerangjang).hide(fragmentKerangjang).commit()
     }
 }
