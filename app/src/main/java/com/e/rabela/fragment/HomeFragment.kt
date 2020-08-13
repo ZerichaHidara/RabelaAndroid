3646723bb95544909620ab7ce88a48df5577ac5b
ZerichaHidara <fendyakakom@gmail.com>
2020-08-14 00:09:21 +0700

    home Seting

--- app/src/main/java/com/e/rabela/fragment/HomeFragment.kt
+++ app/src/main/java/com/e/rabela/fragment/HomeFragment.kt
@@ -6,20 +6,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager.widget.ViewPager
 
 import com.e.rabela.R
+import com.e.rabela.adapter.AdapterSlider
 
 /**
  * A simple [Fragment] subclass.
  */
 class HomeFragment : Fragment() {
 
+    lateinit var slider: ViewPager
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view: View=inflater.inflate(R.layout.fragment_home, container, false)
+
+        slider = view.findViewById(R.id.slider)
+
+        val array = ArrayList<Int>()
+        array.add(R.drawable.slider1)
+        array.add(R.drawable.slider2)
+        array.add(R.drawable.slider3)
+
+        val adapterSlider = AdapterSlider(array, activity)
+        slider.adapter = adapterSlider
+
+        return view
     }
 
 
